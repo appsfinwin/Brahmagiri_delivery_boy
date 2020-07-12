c040ca6c031b665a2a09e49aeaf5ffc61eae92d3
Niyas <niyasnazar23@gmail.com>
2020-07-12 21:38:53 +0530

    TESTFLUTTER PROJECT

--- app/src/main/java/com/example/runner/Fragments/CompletedFragment.java
+++ app/src/main/java/com/example/runner/Fragments/CompletedFragment.java
@@ -55,6 +55,6 @@ public class CompletedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_completed, container, false);
     }
 }
